4fa2cc16d3ed97a0adb87b28bb27889584b1678a
Deepak Kumar <54062595+HakNinja@users.noreply.github.com>
2021-02-03 04:53:16 +0530

    Add files via upload

---
+++ MyAssets/basic/bash-sh.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mv 10/ProgramNameBasic8 10/ProgramNameBasic10
+mv 10/OutputBasic8 10/OutputBasic10
+mv 10/ProgramBasic8 10/ProgramBasic10
+mv 11/ProgramNameBasic8 11/ProgramNameBasic11
+mv 11/OutputBasic8 11/OutputBasic11
+mv 11/ProgramBasic8 11/ProgramBasic11
+mv 12/ProgramNameBasic8 12/ProgramNameBasic12
+mv 12/OutputBasic8 12/OutputBasic12
+mv 12/ProgramBasic8 12/ProgramBasic12
+mv 13/ProgramNameBasic8 13/ProgramNameBasic13
+mv 13/OutputBasic8 13/OutputBasic13
+mv 13/ProgramBasic8 13/ProgramBasic13
+mv 14/ProgramNameBasic8 14/ProgramNameBasic14
+mv 14/OutputBasic8 14/OutputBasic14
+mv 14/ProgramBasic8 14/ProgramBasic14
+mv 15/ProgramNameBasic8 15/ProgramNameBasic15
+mv 15/OutputBasic8 15/OutputBasic15
+mv 15/ProgramBasic8 15/ProgramBasic15
+mv 16/ProgramNameBasic8 16/ProgramNameBasic16
+mv 16/OutputBasic8 16/OutputBasic16
+mv 16/ProgramBasic8 16/ProgramBasic16
+mv 17/ProgramNameBasic8 17/ProgramNameBasic17
+mv 17/OutputBasic8 17/OutputBasic17
+mv 17/ProgramBasic8 17/ProgramBasic17
